e76f76b371f9b12728b2db5c611b117840043ee7
piyush <peeyush0409@gmail.com>
2023-01-04 17:57:10 +0530

    changes applied and database is updated

--- server/Connection.js
+++ server/Connection.js
@@ -20,7 +20,7 @@ connection.query('SELECT * FROM dbt_biding_log', (error, results, fields)=>{
         console.log(error)
     }
   
-    //case 1: If he says to fetch the timestamp from the database
+    // case 1: If he says to fetch the timestamp from the database
     // //fetching the current time data from the db
 
     // for(var i=0; i<results.length; i++){
@@ -28,21 +28,35 @@ connection.query('SELECT * FROM dbt_biding_log', (error, results, fields)=>{
     // }
 
 
-
-
-    //case 2: If he says for the current realtime timestamp
+    var d = new Date()
+    console.log(d.getTime())
+     //This is for the 1 minute time
     const current_time = Date.now()
     console.log(current_time)
 
     //we want the timestamp 1 min before (60 seconds before)
-    const before_time = current_time - 60
+    const before_time = current_time - 60000
     console.log(before_time)
 
     //converting the before time to date format
     const start_date = new Date(before_time)
     console.log(start_date)
 
-    connection.query(`SELECT SUM(complete_qty) as volume,MIN(bid_price) as low,MAX(bid_price) as high,success_time FROM dbt_biding_log WHERE success_time >= '2022-11-30 21:54:08' AND success_time <= '2022-11-30 21:55:08'     market_Symbol='BTC_USDT';`)
+
+    //This is for the 5 mins time
+    const five_mins_ago = current_time - 300000
+    console.log(five_mins_ago)
+
+    console.log("Looping !")
+    //loop 
+    for(var i=five_mins_ago; i<=current_time; i += 60000){
+         
+        const ans = new Date(i)
+        // console.log(ans.toLocaleDateString())
+        // console.log(ans.toLocaleTimeString())
+        console.log(ans.toLocaleString())
+    }
+    // connection.query(`SELECT SUM(complete_qty) as volume,MIN(bid_price) as low,MAX(bid_price) as high,success_time FROM dbt_biding_log WHERE success_time >= '2022-11-30 21:54:08' AND success_time <= '2022-11-30 21:55:08'     market_Symbol='BTC_USDT';`)
 
 })
 
